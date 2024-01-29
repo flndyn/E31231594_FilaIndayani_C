a60cbf9a40cb096ba44b201aa87a409eb71383c2
flndyn <filaindayani9@gmail.com>
2024-01-29 14:44:57 +0700

    Create no5.java

---
+++ no5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fila
+ */
+public class no5 {
+    public static void main(String[] args) {
+int x,y;
+x=10; y=5;
+if(x>y & ++x>10) { System.out.println("TRUE 3 x : " + x);
+System.out.println("TRUE 3 y : " + y);
+System.out.println("TRUE 3 : ");
+}
+else { System.out.println("FALSE 3");
+}
+}
+}
